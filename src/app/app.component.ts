62588b5a3f21fa67b5e8b160d3fd7b693561d2b2
Leo Bruno <leobruno.developer@gmail.com>
2018-11-22 20:29:31 -0300

    Iniciando o projeto - erro na query allUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  constructor(
+    private apollo: Apollo
+  ) {
+    this.createUser();
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    this.apollo.query({
+      query: gql`
+        query {
+          allUsers {
+            id
+            name
+            email
+          }
+        }
+      `
+    }).subscribe(res => console.log('Query: ', res));
+  }
+
+  createUser(): void {
+    this.apollo.mutate({
+      mutation: gql`
+        mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name: 'Lia de Oliveira',
+        email: 'lia@mail.com',
+        password: '123456'
+      }
+    }).subscribe(res => console.log('Mutation: ', res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ApolloConfigModule } from './apollo-config.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    ApolloConfigModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
